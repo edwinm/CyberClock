2d60cb78ae45182997d479867c780ee84ef634be
Edwin Martin <edwin@bitstorm.org>
2025-05-08 00:06:31 +0200

    read BME680 sensor

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,8 @@
 #include "freertos/task.h"
 #include "esp_system.h"
 #include "esp_log.h"
-#include "driver/i2c.h"
+#include "esp_err.h"
+#include "driver/i2c_master.h"
 #include "sdkconfig.h"
 
 #include "freertos/event_groups.h"
@@ -16,6 +17,7 @@
 #include "lwip/sys.h"
 #include "esp_sntp.h"
 #include "time.h"
+#include <bme680.h>
 #include "user_data.h"
 
 #define LGFX_USE_V1
@@ -203,7 +205,7 @@ void set_timezone(void)
     tzset();
 }
 
-extern "C" void app_main(void)
+extern "C" void app_main_old(void)
 {
     ESP_LOGI(TAG, "Initializing display...");
     
@@ -278,4 +280,127 @@ extern "C" void app_main(void)
         // Wait 100ms
         vTaskDelay(100 / portTICK_PERIOD_MS);
     }
+}
+
+static inline void print_registers(bme680_handle_t handle) {
+    /* configuration registers */
+    bme680_control_measurement_register_t ctrl_meas_reg;
+    bme680_control_humidity_register_t    ctrl_humi_reg;
+    bme680_config_register_t              config_reg;
+    bme680_control_gas0_register_t        ctrl_gas0_reg;
+    bme680_control_gas1_register_t        ctrl_gas1_reg;
+
+    /* attempt to read control humidity register */
+    bme680_get_control_humidity_register(handle, &ctrl_humi_reg);
+
+    /* attempt to read control measurement register */
+    bme680_get_control_measurement_register(handle, &ctrl_meas_reg);
+
+    /* attempt to read configuration register */
+    bme680_get_configuration_register(handle, &config_reg);
+
+    /* attempt to read control gas 0 register */
+    bme680_get_control_gas0_register(handle, &ctrl_gas0_reg);
+
+    /* attempt to read control gas 1 register */
+    bme680_get_control_gas1_register(handle, &ctrl_gas1_reg);
+
+    ESP_LOGI(TAG, "Variant Id          (0x%02x): %s", handle->variant_id,uint8_to_binary(handle->variant_id));
+    ESP_LOGI(TAG, "Configuration       (0x%02x): %s", config_reg.reg,    uint8_to_binary(config_reg.reg));
+    ESP_LOGI(TAG, "Control Measurement (0x%02x): %s", ctrl_meas_reg.reg, uint8_to_binary(ctrl_meas_reg.reg));
+    ESP_LOGI(TAG, "Control Humidity    (0x%02x): %s", ctrl_humi_reg.reg, uint8_to_binary(ctrl_humi_reg.reg));
+    ESP_LOGI(TAG, "Control Gas 0       (0x%02x): %s", ctrl_gas0_reg.reg, uint8_to_binary(ctrl_gas0_reg.reg));
+    ESP_LOGI(TAG, "Control Gas 1       (0x%02x): %s", ctrl_gas1_reg.reg, uint8_to_binary(ctrl_gas1_reg.reg));
+}
+
+void i2c0_bme680_task(i2c_master_bus_handle_t i2c0_bus_hdl) {
+    // initialize the xLastWakeTime variable with the current time.
+    TickType_t          last_wake_time  = xTaskGetTickCount ();
+    //
+    // initialize i2c device configuration
+    bme680_config_t dev_cfg         = I2C_BME680_CONFIG_DEFAULT;
+    bme680_handle_t dev_hdl;
+    //
+    // init device
+    bme680_init(i2c0_bus_hdl, &dev_cfg, &dev_hdl);
+    if (dev_hdl == NULL) {
+        ESP_LOGE(TAG, "bme680 handle init failed");
+        assert(dev_hdl);
+    }
+    
+    print_registers(dev_hdl);
+
+    // task loop entry point
+    for ( ;; ) {
+        ESP_LOGI(TAG, "######################## BME680 - START #########################");
+        //
+        // handle sensor
+
+        bme680_data_t data;
+        esp_err_t result = bme680_get_data(dev_hdl, &data);
+        if(result != ESP_OK) {
+            ESP_LOGE(TAG, "bme680 device read failed (%s)", esp_err_to_name(result));
+        } else {
+            data.barometric_pressure = data.barometric_pressure / 100;
+            ESP_LOGI(TAG, "air temperature:     %.2f °C", data.air_temperature);
+            ESP_LOGI(TAG, "dewpoint temperature:%.2f °C", data.dewpoint_temperature);
+            ESP_LOGI(TAG, "relative humidity:   %.2f %%", data.relative_humidity);
+            ESP_LOGI(TAG, "barometric pressure: %.2f hPa", data.barometric_pressure);
+            ESP_LOGI(TAG, "gas resistance:      %.2f kOhms", data.gas_resistance/1000);
+            // ESP_LOGI(TAG, "iaq score:           %u (%s)", data.iaq_score, bme680_iaq_air_quality_to_string(data.iaq_score));
+        }
+        //
+        ESP_LOGI(TAG, "######################## BME680 - END ###########################");
+        //
+        //
+        // pause the task per defined wait period
+        // vTaskDelaySecUntil( &last_wake_time, I2C0_TASK_SAMPLING_RATE );
+        xTaskDelayUntil( &last_wake_time, 1000 / portTICK_PERIOD_MS );
+    }
+    //
+    // free resources
+    bme680_delete( dev_hdl );
+    vTaskDelete( NULL );
+}
+
+
+extern "C" void app_main(void) {
+    vTaskDelay(1500 / portTICK_PERIOD_MS);
+    ESP_LOGI(TAG, "Starting...");
+    vTaskDelay(1500 / portTICK_PERIOD_MS);
+    
+    i2c_master_bus_config_t i2c_mst_config = {
+        .i2c_port = I2C_NUM_0,
+        .sda_io_num = (gpio_num_t)I2C_SDA_PIN,
+        .scl_io_num = (gpio_num_t)I2C_SCL_PIN,
+        .clk_source = I2C_CLK_SRC_DEFAULT,
+        .glitch_ignore_cnt = 7,
+        .intr_priority = 0,
+        .trans_queue_depth = 0,
+        .flags = {
+            .enable_internal_pullup = true,
+            .allow_pd = 0
+        }
+    };
+    
+    i2c_master_bus_handle_t bus_handle;
+    ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_mst_config, &bus_handle));
+
+    i2c0_bme680_task(bus_handle);
+
+    // uint8_t devices_found = 0;
+    
+    // ESP_LOGI(TAG, "Scanning I2C bus...");
+    
+    // for (uint8_t addr = 1; addr < 128; addr++) {
+    //     esp_err_t ret = i2c_master_probe(bus_handle, addr, -1);
+        
+    //     if (ret == ESP_OK) {
+    //         ESP_LOGI(TAG, "Device found at address 0x%02X", addr);
+    //         devices_found++;
+    //     }
+    // }
+    
+    // ESP_LOGI(TAG, "I2C scan completed. Found %d devices.", devices_found);
+    
 }
